6b9acbd82f655c704cc1aab4a3048e962d178180
GiovanniGarlaschini22 <giovannigarlaschini.boolean@gmail.com>
2025-04-15 16:35:19 +0200

    Feat: Esecuzione logica e output completate

--- JS/script.js
+++ JS/script.js
@@ -32,3 +32,19 @@
 
 // OUTPUT //
 // I multipli sia di 3 che di 5 vengono stampati "FizzBuzz", I multipli di 3 vengono stampati "Fizz", I multipli di 5 vengono stampati "Buzz";
+
+
+
+// ESECUZIONE LOGICA //
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");  
+    } else if (i % 5 === 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i); 
+    }
+}
